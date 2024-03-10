d06856e3bc3df8561ffdffd4f1908e8dcebc8c8c
Miranda <fmiranda@bankly.com.br>
2024-03-10 13:33:39 -0300

    fix: use correct output

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 output "tech_challenge_user_pool_id" {
-  value = module.aws-cognito.tech_challenge_user_pool_client_id
+  value = module.aws-cognito.tech_challenge_user_pool_id
 }
 
 output "tech_challenge_user_pool_client_id" {
