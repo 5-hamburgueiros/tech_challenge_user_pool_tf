232129db0aaf4975c73250a8af189e97b19d0176
Miranda <fmiranda@bankly.com.br>
2024-03-10 13:30:16 -0300

    fix: use correct user pool id

--- infra/aws-cognito/outputs.tf
+++ infra/aws-cognito/outputs.tf
@@ -1,5 +1,5 @@
 output "tech_challenge_user_pool_id" {
-  value = aws_cognito_user_pool.tech_challenge_user_pool.pool_id
+  value = aws_cognito_user_pool.tech_challenge_user_pool.id
 }
 
 output "tech_challenge_user_pool_client_id" {
